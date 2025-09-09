6775e58d72354d2ef35c38b1f25211ff3ae9acc2
s.aran <57449457+s-aran@users.noreply.github.com>
2025-09-09 20:42:37 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use ahash::AHashSet;
 use clap::{Parser, Subcommand};
+use dialoguer::Input;
 use dmdr_core::model::{MetaData, MyField, MyModel, RelationType};
 use std::io::{stdout, BufWriter, Write};
 use std::sync::Arc;
@@ -32,6 +33,8 @@ enum Commands {
         uuid: bool,
         #[clap(value_name = "MODEL")]
         model: Option<String>,
+        #[clap(long = "fields")]
+        fields: bool,
     },
     Write {
         #[clap(value_name = "MODEL")]
@@ -58,22 +61,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (data, indexes) = load_json(args.file.into())?;
 
     match args.command {
-        Commands::Enumerate { uuid, model } => {
-            let (data, indexes) = if let Some(model) = model {
-                if let Some(model) = get_model_by(&indexes, model.as_str()) {
-                    let uuid = model._meta_data.uuid.clone();
-                    rebuild(data, indexes, uuid)
-                } else {
-                    panic!("no match {} in models", model);
-                }
-            } else {
-                (data, indexes)
-            };
-
-            let lines = enumerate(&data, &indexes, uuid);
-            let mut out = BufWriter::new(stdout().lock());
-            write(&mut out, lines.join("\n").as_bytes());
-            println!("");
+        Commands::Enumerate {
+            uuid,
+            model,
+            fields,
+        } => {
+            let instance = CommandEnumerate::new(uuid, model, fields);
+            instance.command(&data, &indexes);
         }
         Commands::Write { model } => {
             let (data, indexes) = if let Some(model) = model {
@@ -95,39 +89,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             show_fields,
             show_meta,
             show_source,
-        } => {
-            if let Some(model) = get_model_by(&indexes, model.as_str()) {
-                let mut lines = get_display_models(&model);
-                if show_fields {
-                    for f in model.local_fields.iter() {
-                        lines.extend(get_display_fields(&indexes, f, FieldType::Local));
-                    }
-
-                    for f in model.relation_fields.iter() {
-                        lines.extend(get_display_fields(&indexes, f, FieldType::Related));
-                    }
-
-                    for f in model.forward_fields.iter() {
-                        lines.extend(get_display_fields(&indexes, f, FieldType::Forwarded));
-                    }
-                }
-
-                if show_meta {
-                    lines.extend(get_display_meta_data(&model._meta_data));
-                }
-
-                if show_source {
-                    lines.push(model._meta_data.code.partial.concat());
-                }
-
-                lines.push("".to_owned());
-
-                let mut out = BufWriter::new(stdout().lock());
-                write(&mut out, lines.join("\n").as_bytes());
-            } else {
-                panic!("no match {} in models", model);
-            }
-        }
+        } => {}
         Commands::Interactive => {
             let mut out = BufWriter::new(stdout().lock());
             write(&mut out, "> ".as_bytes());
@@ -194,7 +156,12 @@ where
     let _ = to.flush();
 }
 
-fn enumerate(data: &Structure, indexes: &UuidIndexes, show_uuid: bool) -> Vec<String> {
+fn enumerate(
+    data: &Structure,
+    indexes: &UuidIndexes,
+    show_uuid: bool,
+    show_fields: bool,
+) -> Vec<String> {
     let mut lines: Vec<String> =
         Vec::with_capacity(indexes.get_models().len() + indexes.get_fields().len());
 
@@ -206,6 +173,10 @@ fn enumerate(data: &Structure, indexes: &UuidIndexes, show_uuid: bool) -> Vec<St
             ));
         } else {
             lines.push(format!("[M] {}", model.object_name));
+        }
+
+        if !show_fields {
+            continue;
         }
 
         for field in &model.local_fields {
@@ -529,15 +500,97 @@ fn interactive(data: &Structure, indexes: &UuidIndexes) -> bool {
     let args: Vec<String> = cmd_args_iter.collect();
     match cmd.to_lowercase().as_str() {
         "get" => {
-            //
+            // get
             return true;
         }
         "exit" | "quit" => false,
         _ => {
-            println!("");
             let msg = format!("unexpected command: {}", cmd);
             println!("{}", msg);
             return true;
+        }
+    }
+}
+
+trait Command {
+    fn command(&self, data: &Arc<Structure>, indexes: &UuidIndexes);
+}
+
+struct CommandEnumerate {
+    uuid: bool,
+    model: Option<String>,
+    fields: bool,
+}
+
+impl CommandEnumerate {
+    pub fn new(uuid: bool, model: Option<String>, fields: bool) -> Self {
+        Self {
+            uuid,
+            model,
+            fields,
+        }
+    }
+}
+
+impl Command for CommandEnumerate {
+    fn command(&self, data: &Arc<Structure>, indexes: &UuidIndexes) {
+        let (data, indexes) = if let Some(ref model_name) = self.model {
+            if let Some(model) = get_model_by(&indexes, model_name.as_str()) {
+                let uuid = model._meta_data.uuid.clone();
+                rebuild(Arc::clone(data), indexes.clone(), uuid)
+            } else {
+                panic!("no match {} in models", model_name);
+            }
+        } else {
+            (Arc::clone(data), indexes.clone())
+        };
+
+        let lines = enumerate(&data, &indexes, self.uuid, self.fields);
+        let mut out = BufWriter::new(stdout().lock());
+        write(&mut out, lines.join("\n").as_bytes());
+        println!("");
+    }
+}
+
+struct CommandGet {
+    model: String,
+    show_fields: bool,
+    show_meta: bool,
+    show_source: bool,
+}
+
+impl Command for CommandGet {
+    fn command(&self, data: &Arc<Structure>, indexes: &UuidIndexes) {
+        if let Some(model) = get_model_by(&indexes, self.model.as_str()) {
+            let mut lines = get_display_models(&model);
+            if self.show_fields {
+                for f in model.local_fields.iter() {
+                    lines.extend(get_display_fields(&indexes, f, FieldType::Local));
+                }
+
+                for f in model.relation_fields.iter() {
+                    lines.extend(get_display_fields(&indexes, f, FieldType::Related));
+                }
+
+                for f in model.forward_fields.iter() {
+                    lines.extend(get_display_fields(&indexes, f, FieldType::Forwarded));
+                }
+            }
+
+            if self.show_meta {
+                lines.extend(get_display_meta_data(&model._meta_data));
+            }
+
+            if self.show_source {
+                lines.push(model._meta_data.code.partial.concat());
+            }
+
+            lines.push("".to_owned());
+
+            let mut out = BufWriter::new(stdout().lock());
+            write(&mut out, lines.join("\n").as_bytes());
+        } else {
+            panic!("no match {} in models", self.model);
         }
     }
 }
